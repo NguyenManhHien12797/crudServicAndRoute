34b227fdee5928b66e9c2de343f33d4cb1a3f168
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-10-24 09:54:27 +0700

    add

--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -9,31 +9,35 @@ import {Product} from "../../model/product";
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  productForm: FormGroup;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
+   const product = this.getProduct();
+    this.productForm = new FormGroup({
+      id: new FormControl(product.id),
+      name: new FormControl(product.name),
+      price: new FormControl(product.price),
+      description: new FormControl(product.description),
+    })
+  }
 
   ngOnInit(): void {
-    this.getProduct();
+
   }
 
-  product: Product = {
-    id: 1,
-    name: '',
-    price: 1,
-    description: ''
-  };
+  // product: Product = {
+  //   id: 1,
+  //   name: '',
+  //   price: 1,
+  //   description: ''
+  // };
 
   getProduct(){
-    this.product = this.productService.getProduct();
+    return  this.productService.getProduct();
   }
 
 
-  productForm: FormGroup = new FormGroup({
-    id: new FormControl(),
-    name: new FormControl(),
-    price: new FormControl(),
-    description: new FormControl(),
-  })
+
 
   submit(){
     const product= this.productForm.value;
